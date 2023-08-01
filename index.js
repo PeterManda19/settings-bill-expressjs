8a74d264d8c462aef87f9982d222f472cebb7cca
Peter <peter.pmanda.manda@gmail.com>
2023-08-01 10:03:46 +0200

    localhost:3011 showing Bill with settings page

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 let app = express();
 
+// Set up Handlebars view engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+// Set the 'main' layout as the default layout
+app.set('views', './views');
+app.set('view options', { layout: 'main' });
+
+// Serve static resources from the 'public' folder
+app.use(express.static('public'));
+
 // GET route '/'
 app.get('/', function(req, res){
-  res.send('Settings Bill App')
+  res.render('index')
 });
 
 // POST route '/settings'
@@ -13,10 +25,19 @@ app.post('/settings', function(req, res){
 });
 
 // POST route '/action'
+app.post('/action', function(req, res){
+  res.send('Settings Bill App')
+});
 
 // GET route '/actions'
+app.post('/actions', function(req, res){
+  res.send('Settings Bill App')
+});
 
 // GET route '/actions/:type'
+app.post('/actiions/:type  ', function(req, res){
+  res.send('Settings Bill App')
+});
 
 const PORT = process.env.PORT || 3011;
 
