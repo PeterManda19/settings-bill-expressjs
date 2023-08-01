e632ef37fa06a602c5d58a39adb34cd41c4607ba
Peter <peter.pmanda.manda@gmail.com>
2023-08-01 08:54:22 +0200

    working on GET and POST routes

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ import express from 'express';
 
 let app = express();
 
+// GET route '/'
 app.get('/', function(req, res){
   res.send('Settings Bill App')
 });
 
+// POST route '/settings'
+app.post('/settings', function(req, res){
+  res.send('Settings Bill App')
+});
+
+// POST route '/action'
+
+// GET route '/actions'
+
+// GET route '/actions/:type'
+
+const PORT = process.env.PORT || 3011;
+
 app.listen(3011, function(){
-  console.log("App started")
+  console.log("App started at port:", PORT)
 });
