2562a3d48f5be7c78b51ba6199ef1722f1db994f
Peter <peter.pmanda.manda@gmail.com>
2023-08-10 17:38:14 +0200

    Error: ENOENT: no such file or directory resolved

--- index.js
+++ index.js
@@ -2,31 +2,34 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 import SettingsBill from './settings-bill.js';
 import bodyParser from 'body-parser';
+import { fileURLToPath} from 'url';
+import path from 'path'; 
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 var app = express();
 var settingsBill = SettingsBill();
 
 // Set up Handlebars view engine
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
 app.set('view engine', 'handlebars');
 
 // Set the 'main' layout as the default layout
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view options', { layout: 'main' });
 
 // Serve static resources from the 'public' folder
 app.use(express.static('public'));
-
-// // Instead of body-parser, use inbuilt express.json() and express.urlencoded()
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
-
 
 // GET route '/'
 app.get('/', function(req, res){
