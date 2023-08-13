2eccafd186888ebec0e1b6b0aa6366aa53e8b1d0
Peter <peter.pmanda.manda@gmail.com>
2023-08-14 01:29:40 +0200

    updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 import { fileURLToPath} from 'url';
 import path from 'path'; 
 import { dirname } from 'path';
-import moment from 'moment'; // Import the moment library
+//import moment from 'moment'; // Import the moment library
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
